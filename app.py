ebafb8ec1d7f087ee72be8a642865b00ad7800bf
Bockslunch <155188366+Bockslunch@users.noreply.github.com>
2025-03-18 21:08:44 -0600

    Update app.py - fixed ''') bug

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def inserting():
         ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
         ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
         {'Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
